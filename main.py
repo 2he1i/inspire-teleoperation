1cff6c4fa3d6cc5243a43b48cbd6168c418520a9
2he1i <2he1i.tech99@gmail.com>
2026-07-21 19:13:16 +0800

    Organize ignores and add launcher

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Run the Inspire hand teleoperation application from the repository root."""
+
+from inspire_teleoperation.main import main
+
+
+if __name__ == "__main__":
+    main()
